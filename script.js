4b1c6a54e918b119e24063fc89da4c4589229d3a
Aleks40444 <240106@turan-edu.kz>
2026-05-28 11:27:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 
+<script>
+        // тут снова ничего нееееет
+        function updateClock() {
+            const clockEl = document.getElementById('clock');
+            const now = new Date();
+            clockEl.innerText = now.toTimeString().split(' ')[0];
+        }
+        setInterval(updateClock, 1000);
+        updateClock();
+
+        // ой, тут тоже ничего нет
+        const consoleEl = document.getElementById('console');
+        function addLog(text) {
+            const line = document.createElement('div');
+            line.className = 'log-line';
+            line.innerText = `> ${text}`;
+            consoleEl.appendChild(line);
+
+            // Что-то ищете? а тут ничегог нет
+            if (consoleEl.children.length > 5) {
+                consoleEl.removeChild(consoleEl.children[0]);
+            }
+        }
+
+        // ничего нет
+        const fakeErrors = [
+            "Предупреждение: Ничего не обнаружено.",
+            "Сканирование памяти... пусто.",
+            "Пакет данных утерян...",
+            "Что?",
+            "Внимание: Ожидание клика."
+        ];
+
+        setInterval(() => {
+            const randomText = fakeErrors[Math.floor(Math.random() * fakeErrors.length)];
+            addLog(randomText);
+        }, 4000);
+        /* Это конец и тут тоже ничего нет */
+    </script>
